c6b5f281d6c1c51f3734c9ed681aa41b4dc5cbec
Abolfazl <ryan.abolfazl@gmail.com>
2025-12-14 22:55:00 +0330

    added logout & messages.html with toastify

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('', views.LoginView.as_view(), name='login'),
-    # path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     # path('register/', views.RegisterView.as_view(), name='register'),
     # path('', include('django.contrib.auth.urls')),
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -6,3 +6,6 @@ class LoginView(auth_views.LoginView):
     form_class = AuthenticationForm
     template_name = 'accounts/login.html'
     redirect_authenticated_user = True
+
+class LogoutView(auth_views.LogoutView):
+    pass
