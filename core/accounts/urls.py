529447e40087380c22fb416209658143b7de4f21
Abolfazl <ryan.abolfazl@gmail.com>
2025-12-14 22:28:10 +0330

    added login view

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'accounts'
 urlpatterns = [
+    path('', views.LoginView.as_view(), name='login'),
+    # path('logout/', views.LogoutView.as_view(), name='logout'),
+    # path('register/', views.RegisterView.as_view(), name='register'),
+    # path('', include('django.contrib.auth.urls')),
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import views as auth_views
+from .forms import AuthenticationForm
 
-# Create your views here.
+class LoginView(auth_views.LoginView):
+    form_class = AuthenticationForm
+    template_name = 'accounts/login.html'
+    redirect_authenticated_user = True
