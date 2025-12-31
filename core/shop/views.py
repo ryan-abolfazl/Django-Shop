a436154cbfcc96cb0e07b7fbaf2d9295e2598724
Abolfazl <ryan.abolfazl@gmail.com>
2025-12-31 21:22:45 +0330

    added filters for django list view

--- core/shop/views.py
+++ core/shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.core.exceptions import FieldError
 from django.views.generic import (
     TemplateView,
     ListView,
@@ -10,12 +10,25 @@ class ProductGridView(ListView):
     template_name = 'shop/product-grid.html'
     paginate_by = 9
 
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get("page_size", self.paginate_by)
+
     def get_queryset(self):
         queryset = ProductModel.objects.filter(status=ProductStatusType.publish.value)
         if search_q:=self.request.GET.get("q"):
             queryset = queryset.filter(title__icontains=search_q)
         if category_id:=self.request.GET.get("category_id"):
             queryset = queryset.filter(category__id=category_id)
+        if min_price:=self.request.GET.get("min_price"):
+            queryset = queryset.filter(price__gte=min_price)
+        if max_price:=self.request.GET.get("max_price"):
+            queryset = queryset.filter(price__lte=max_price)
+
+        if order_by:=self.request.GET.get("order_by"):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
 
         return queryset
 
