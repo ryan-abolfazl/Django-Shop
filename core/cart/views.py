0630e6c0a962788077d857f7ec47ba47ccc4c05e
Abolfazl <ryan.abolfazl@gmail.com>
2026-01-07 13:28:05 +0330

    update cart and remove object

--- core/cart/views.py
+++ core/cart/views.py
@@ -3,16 +3,35 @@ from django.views.generic import View, TemplateView
 from django.http import HttpResponse, JsonResponse
 from .cart import CartSession
 from typing import Any
-class SessionAddProduct(View):
+class SessionAddProductView(View):
 
     def post(self, request, *args, **kwargs):
         cart = CartSession(request.session)
         product_id = request.POST.get("product_id")
         if product_id:
             cart.add_product(product_id)
-        return JsonResponse({"cart":cart.get_cart_dict()})
+        return JsonResponse({"cart":cart.get_cart_dict(),"total_quantity":cart.get_total_quantity()})
 
-class SessionCartSummary(TemplateView):
+class SessionRemoveProductView(View):
+
+    def post(self, request, *args, **kwargs):
+        cart = CartSession(request.session)
+        product_id = request.POST.get("product_id")
+        if product_id:
+            cart.remove_product(product_id)
+        return JsonResponse({"cart":cart.get_cart_dict(),"total_quantity":cart.get_total_quantity()})
+
+class SessionUpdateProductQuantityView(View):
+
+    def post(self, request, *args, **kwargs):
+        cart = CartSession(request.session)
+        product_id = request.POST.get("product_id")
+        quantity = int(request.POST.get("quantity"))
+        if product_id and quantity:
+            cart.update_product_quantity(product_id, quantity)
+        return JsonResponse({"cart":cart.get_cart_dict(),"total_quantity":cart.get_total_quantity()})
+
+class SessionCartSummaryView(TemplateView):
     template_name = "cart/cart-summary.html"
 
     def get_context_data(self, **kwargs:Any):
