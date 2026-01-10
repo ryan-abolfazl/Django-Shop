59d1965722b8c09fc8e565985757c1eef2322410
Abolfazl <ryan.abolfazl@gmail.com>
2026-01-10 21:38:40 +0330

    added permissions in permissions.py

--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from dashboard.permissions import HasAdminAccessPermission
 
-class AdminDashboardHomeView(TemplateView):
+class AdminDashboardHomeView(HasAdminAccessPermission,LoginRequiredMixin, TemplateView):
     template_name = "dashboard/admin/home.html"
--- core/dashboard/customer/views.py
+++ core/dashboard/customer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
-
-class CustomerDashboardHomeView(TemplateView):
+from django.contrib.auth.mixins import LoginRequiredMixin
+from dashboard.permissions import HasCustomerAccessPermission
+class CustomerDashboardHomeView(LoginRequiredMixin,HasCustomerAccessPermission, TemplateView):
     template_name = "dashboard/customer/home.html"
