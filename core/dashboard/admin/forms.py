e9a3edf2dbe32c04ee516ab939882928e9793fee
Abolfazl <ryan.abolfazl@gmail.com>
2026-01-11 00:04:58 +0330

    added security edit page

---
+++ core/dashboard/admin/forms.py
@@ -0,0 +1,4 @@
+from django.contrib.auth import forms as auth_forms
+from django import forms
+class AdminPasswordChangeForm(auth_forms.PasswordChangeForm):
+    pass
--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.views.generic import View, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from dashboard.permissions import HasAdminAccessPermission
-
-class AdminDashboardHomeView(HasAdminAccessPermission,LoginRequiredMixin, TemplateView):
+from django.contrib.auth import views as auth_views
+from dashboard.admin.forms import AdminPasswordChangeForm
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+class AdminDashboardHomeView(LoginRequiredMixin, HasAdminAccessPermission, TemplateView):
     template_name = "dashboard/admin/home.html"
+
+
+class AdminSecurityEditView(LoginRequiredMixin, HasAdminAccessPermission,SuccessMessageMixin, auth_views.PasswordChangeView):
+    template_name = "dashboard/admin/profile/security-edit.html"
+    form_class = AdminPasswordChangeForm
+    success_url = reverse_lazy("dashboard:admin:security-edit")
+    success_message = 'بروزرسانی با موفقیت انجام شد'
