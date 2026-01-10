fb616c7a6b1084fc0c9877fa1065f61a8ea0766b
Abolfazl <ryan.abolfazl@gmail.com>
2026-01-11 00:29:24 +0330

    added form error messages

--- core/dashboard/admin/forms.py
+++ core/dashboard/admin/forms.py
@@ -1,4 +1,18 @@
 from django.contrib.auth import forms as auth_forms
 from django import forms
+from django.utils.translation import gettext_lazy as _
 class AdminPasswordChangeForm(auth_forms.PasswordChangeForm):
-    pass
+    error_messages = {
+        "password_incorrect": _(
+            "پسورد قبلی شما اشتباه وارد شده است. لطفا تصحیح نمایید."
+        ),
+        "password_mismatch": _("دو پسورد ورودی با یکدیگر مطابقت ندارند."),
+    }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["old_password"].widget.attrs["class"] = 'form-control text-center'
+        self.fields["new_password1"].widget.attrs["class"] = 'form-control text-center'
+        self.fields["new_password2"].widget.attrs["class"] = 'form-control text-center'
+        self.fields["old_password"].widget.attrs["placeholder"] = 'پسورد قبلی خود را وارد نمایید'
+        self.fields["new_password1"].widget.attrs["placeholder"] = 'پسورد جدید خود را وارد نمایید'
+        self.fields["new_password2"].widget.attrs["placeholder"] = 'پسورد جدید خود را مجددا وارد نمایید'
