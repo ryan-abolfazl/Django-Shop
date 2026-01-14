c40d6552f3efbba18d5eaf06b01340c4fd292a6c
Abolfazl <ryan.abolfazl@gmail.com>
2026-01-14 22:44:31 +0330

    added order urls, views, forms in customer dashboard

---
+++ core/dashboard/customer/forms/addresses.py
@@ -0,0 +1,13 @@
+from django import forms
+from order.models import UserAddressModel
+
+class UserAddressForm(forms.ModelForm):
+    class Meta:
+        model = UserAddressModel
+        fields = [
+            "address",
+            "state",
+            "city",
+            "zip_code",
+
+        ]
---
+++ core/dashboard/customer/views/addresses.py
@@ -0,0 +1,84 @@
+from django.db.models.query import QuerySet
+from django.forms.forms import BaseForm
+from django.views.generic import (
+    UpdateView,
+    ListView,
+    DeleteView,
+    CreateView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from order.permissions import HasCustomerAccessPermission
+from django.contrib.auth import views as auth_views
+from dashboard.customer.forms import *
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from accounts.models import Profile
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.core.exceptions import FieldError
+from ..forms import UserAddressForm
+from order.models import UserAddressModel
+
+class CustomerAddressListView(LoginRequiredMixin, HasCustomerAccessPermission, ListView):
+    template_name = 'dashboard/customer/addresses/address-list.html'
+    paginate_by = 10
+
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get("page_size", self.paginate_by)
+
+    def get_queryset(self):
+        queryset = UserAddressModel.objects.filter(user=self.request.user)
+        if search_q:=self.request.GET.get("q"):
+            queryset = queryset.filter(title__icontains=search_q)
+        if order_by:=self.request.GET.get("order_by"):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
+
+        return queryset
+
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context["total_items"] = self.get_queryset().count()
+        return context
+
+class CustomerAddressCreateView(LoginRequiredMixin, HasCustomerAccessPermission, SuccessMessageMixin, CreateView):
+    template_name = 'dashboard/customer/addresses/address-create.html'
+    form_class = UserAddressForm
+    success_message = "ایجاد آدرس با موفقیت انجام شد"
+
+    def get_queryset(self):
+        queryset = UserAddressModel.objects.filter(user=self.request.user)
+        return queryset
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+    def get_success_url(self):
+        return reverse_lazy("dashboard:customer:address-list")
+
+
+class CustomerAddressUpdateView(LoginRequiredMixin, HasCustomerAccessPermission, SuccessMessageMixin, UpdateView):
+    template_name = 'dashboard/customer/addresses/address-edit.html'
+    form_class = UserAddressForm
+    success_message = "ویرایش آدرس با موفقیت انجام شد"
+
+    def get_queryset(self):
+        queryset = UserAddressModel.objects.filter(user=self.request.user)
+        return queryset
+
+    def get_success_url(self):
+        return reverse_lazy("dashboard:customer:address-edit", kwargs={"pk":self.get_object().pk})
+
+class CustomerAddressDeleteView(LoginRequiredMixin, HasCustomerAccessPermission, SuccessMessageMixin, DeleteView):
+    template_name = 'dashboard/customer/addresses/address-delete.html'
+    success_url = reverse_lazy("dashboard:customer:address-list")
+    success_message = "حذف آدرس با موفقیت انجام شد"
+
+    def get_queryset(self):
+        queryset = UserAddressModel.objects.filter(user=self.request.user)
+        return queryset
